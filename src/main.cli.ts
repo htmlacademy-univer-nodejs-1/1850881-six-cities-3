8db6cb3d2099eae32e929a3cfce484cee94a7f9e
Ivan Sychev <funovichf@gmail.com>
2024-11-10 23:33:47 +0500

    module 4 done

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -6,6 +6,7 @@ import VersionCommand from './core/cli-command/version.command.js';
 import ImportCommand from './core/cli-command/import.command.js';
 import GenerateCommand from './core/cli-command/generate-command.js';
 
+// sorry :)
 const myManager = new CliApplication();
 myManager.registerCommands([new HelperCommand(), new VersionCommand(), new ImportCommand(), new GenerateCommand()]);
 myManager.processCommand(process.argv);
